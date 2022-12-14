c2eef3912e92fa64fc4751828a46cac2e3307e88
TristanBrattinga <96078643+TristanBrattinga@users.noreply.github.com>
2022-12-14 13:07:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
-// JavaScript Document
-console.log("hi");
+var burgerBtn = document.querySelector("header nav button");
+var andereBody = document.querySelector("body")
+
+burgerBtn.addEventListener("click", burgerBtnClick);
+
+function burgerBtnClick() {
+    burgerBtn.classList.toggle("showMenu");
+    andereBody.classList.toggle("hamburgerMenu")
+}
+
